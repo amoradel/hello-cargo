8053b062ad51360b9727dfd122bfb9e19ef4929d
Anibal Moradel <amoradel@outlook.com>
2023-04-28 11:47:12 -0600

    feat: :sparkles: Variables (mutable and inmutable)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    let name: &str = "Anibal"; //Inmutable (como una variable static)
+    let mut age: i32 = 30; //mMtable (que se puede cambiar el valor)
+    print!("Hola {}, Tu edad es {}, feliciades\n\n", name, age);
+    age = 32;
+    print!("Hola {}, Tu edad es {}, feliciades\n\n", name, age);
+
+    // todo!("Este es un todo por hacer");
 }
